1f18bf3f9a383261cb3dab6345fa998268f5ce53
selvinmedinawork <selvinmedinawork@gmail.com>
2021-10-25 17:36:29 -0600

    Add project files.

---
+++ Test07/Controllers/ConexionApiTest06Controller.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Test07.Controllers
+{
+    [ApiController]
+    [Route("ObtenerSaludoDesdeOtraApi")]
+    public class ConexionApiTest06Controller : ControllerBase
+    {
+        private readonly RestClient _restClient;
+
+        public ConexionApiTest06Controller(IConfiguration configuration)
+        {
+            _restClient = new RestClient();
+
+            var test07Api = configuration["api:Test07"];
+
+            _restClient.BaseUrl = new Uri(test07Api);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerSaludo()
+        {
+            try
+            {
+                var request = new RestRequest("ObtenerSaludoDesdeOtraApi", Method.GET);
+
+                var respuesta = await _restClient.ExecuteGetAsync(request);
+
+                return Ok(respuesta.Content);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, JsonConvert.SerializeObject(ex));
+            }
+        }
+    }
+}
